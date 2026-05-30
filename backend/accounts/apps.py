3dc484f5fbfc83a59fa3a8c232960b7e002a423d
Meghasyam99 <uppumeghasyam16@gmail.com>
2026-05-30 13:19:37 +0530

    Update backend fixes

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
+
+# --- TEMPORARY: Create a superuser on production deploy ---
+def create_superuser_once():
+    User = get_user_model()
+    if not User.objects.filter(username="prodadmin").exists():
+        User.objects.create_superuser(
+            username="prodadmin",
+            email="prodadmin@example.com",
+            password="ProdSuperSecret123!"
+        )
+create_superuser_once()
+# --- END TEMPORARY ---
 from django.apps import AppConfig
 
 
